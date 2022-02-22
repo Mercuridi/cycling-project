f81253e0ddb153cfbecdf0c503d5a5264c46fd94
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2022-02-22 19:35:17 +0000

    added riderName attribute

--- Rider.java
+++ Rider.java
@@ -1,6 +1,7 @@
 public class Rider {
     //attributes
     private int playerID; //example: 1111
+    private String riderName; // example: Vincenzo Nibali
     //getters and setters
     //methods
     //constructors

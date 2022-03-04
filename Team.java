bafe51e4528fb542c2f12203a0dbd5bd51f9706b
LozzaCodes <laurieharbord@clara.co.uk>
2022-03-04 23:02:10 +0000

    added Team

---
+++ Team.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Team {
+    //attributes
+    private static int NextTeamID;
+    private int teamID;
+    private String teamName;
+    private ArrayList ridersInTeam;
+    private String teamDescription;
+    //getters and setters
+    public int getTeamID() {
+        return teamID;
+    }
+    public String getTeamName() {
+        return teamName;
+    }
+    public String getTeamDescription() {
+        return teamDescription;
+    }
+    public ArrayList getRidersInTeam() {
+        return ridersInTeam;
+    }
+    //methods
+
+    //constructors
+}

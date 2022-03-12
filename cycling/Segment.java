ba143d37f6b7485b6b28fdfe43fcbe000157e057
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2022-03-12 15:28:13 +0000

    add getters and setters to Segment

--- cycling/Segment.java
+++ cycling/Segment.java
@@ -11,4 +11,74 @@ public class Segment{
     private SegmentType segmentType;
     // private ArrayList<SegmentResult> segmentResults;
     private double segmentLength;
+    private Double segmentLocation;
+    private Double averageGradient;
+
+public int getnextSegmentID(){
+    return nextSegmentID;
+}
+
+public int getsegmentID(){
+    return segmentID;
+}
+
+public int getstageID(){
+    return stageID;
+}
+
+public int getraceID(){
+    return raceID;
+}
+
+public double getsegmentLength(){
+    return segmentLength;
+}
+
+public String getsegmentName(){
+    return segmentName;
+}
+
+public SegmentType getsegmentType(){
+    return segmentType;
+}
+
+public double getsegmentLocation(){
+    return segmentLocation;
+}
+
+public double getaverageGradient(){
+    return averageGradient;
+}
+
+public void setsegmentID(int segmentID){
+    this.segmentID = segmentID;
+}
+
+public void setstageID(int stageID){
+    this.stageID = stageID;
+}
+
+public void setraceID(int raceID){
+    this.raceID = raceID;
+}
+
+public void setsegmentLength(double segmentLength){
+    this.segmentLength = segmentLength;
+}
+
+public void setsegmentName(String segmentName){
+    this.segmentName = segmentName;
+}
+
+public void setsegmentType(SegmentType segmentType){
+    this.segmentType = segmentType;
+}
+
+public void setsegmentLocation(double segmentLocation){
+    this.segmentLocation = segmentLocation;
+}
+
+public void setaverageGradient(double averageGradient){
+    this.averageGradient = averageGradient;
+}
 }

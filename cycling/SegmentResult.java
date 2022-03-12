853a5d5b94fbd3e38b1c11b4d2963563200d7861
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2022-03-12 15:45:15 +0000

    add Results classes, attributes, getters, setters

---
+++ cycling/SegmentResult.java
@@ -0,0 +1,12 @@
+package cycling;
+public class SegmentResult extends StageResult{
+    public int segmentID;
+
+public int getsegmentID(){
+    return segmentID;
+}
+
+public void setsegmentID(int segmentID){
+    this.segmentID = segmentID;
+}
+}
---
+++ cycling/StageResult.java
@@ -0,0 +1,12 @@
+package cycling;
+public class StageResult extends RaceResult {
+    public int stageID;
+
+public int getstageID(){
+    return stageID;
+}
+
+public void setstageID(int stageID){
+    this.stageID = stageID;
+}
+}

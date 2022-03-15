7ac44e5fb5a919a3de6bae5a2a093032eb9aaae7
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2022-03-15 21:30:15 +0000

    push results arrays

--- cycling/CyclingPortal.java
+++ cycling/CyclingPortal.java
@@ -14,6 +14,10 @@ import java.util.ArrayList;
 public class CyclingPortal implements CyclingPortalInterface {
 	public ArrayList<Team> Teams = new ArrayList<Team>();
 	public ArrayList<Race> Races = new ArrayList<Race>();
+	public ArrayList<RaceResult> RaceResults = new ArrayList<RaceResult>();
+	public ArrayList<StageResult> StageResults = new ArrayList<StageResult>();
+	public ArrayList<SegmentResult> SegmentResults = new ArrayList<SegmentResult>();
+
 
 	//our own methods!!!
 	public int locateTeam(int teamID) throws IDNotRecognisedException {
